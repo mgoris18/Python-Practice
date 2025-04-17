254ea161535b9072f1ecda4728dfcc4203614cf1
Michell <mgoris18@gmail.com>
2025-04-17 17:51:08 -0400

    task 2

---
+++ Add Two Numbers.py
@@ -0,0 +1,11 @@
+#ask user to input first number
+num1 = int(input("Input the first number: "))
+
+#ask user to input second number
+num2 = int(input("Input the second number: "))
+
+#add the two numbers and save them in the 'total' variable
+total = num1+num2
+
+#print the result
+print("The sum of these two numbers is:", total )
